691567aa72a17142915e354a52b235a523d3f800
stephan <stephanmh@gmail.com>
2020-06-12 19:31:46 +0200

    feat(ghpages): inital test.

--- release.config.js
+++ release.config.js
@@ -25,7 +25,14 @@ module.exports = {
         ,
         ["@semantic-release/git", {
             "assets": ["docs/CHANGELOG.md","package.json"],
-        }]
+        }],
+        [
+            "@qiwi/semantic-release-gh-pages-plugin",
+            {
+                "msg": "updated",
+                "src": "dist"
+            }
+        ]
     ],
     "preset": "angular"
 }
